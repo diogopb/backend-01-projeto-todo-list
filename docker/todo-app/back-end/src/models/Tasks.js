d90a1cee635d6470f9a44fa75971aad3cf3893ff
Diogo Pinto <dpintobarbosa@gmail.com>
2024-03-20 09:55:48 -0300

    Requisito 1

--- docker/todo-app/back-end/src/models/Tasks.js
+++ docker/todo-app/back-end/src/models/Tasks.js
@@ -1,23 +1,31 @@
-const { readFile, writeFile } = require('../utils/fileHandler');
-const uuid = require('uuid').v4;
+const { readFile, writeFile } = require("../utils/fileHandler");
+const uuid = require("uuid").v4;
 
 const jsonDBPath = "./src/database/";
 
-const getAllTasks = async () => readFile(`${jsonDBPath}tasks.json`)
-  .then((file)=> JSON.parse(file))
-  .catch(({ message })=> new Error(`Não foi possível consultar o banco de dados:\n${message}`));
+const getAllTasks = async () =>
+  readFile(`${jsonDBPath}tasks.json`)
+    .then((file) => JSON.parse(file))
+    .catch(
+      ({ message }) =>
+        new Error(`Não foi possível consultar o banco de dados:\n${message}`)
+    );
 
-const getTask = async (id) => getAllTasks()
-  .then((tasks)=> tasks.filter(item=> item.id === id))
-  .catch(({ message })=> new Error(`Não foi possível consultar o banco de dados:\n${message}`));
+const getTask = async (id) =>
+  getAllTasks()
+    .then((tasks) => tasks.filter((item) => item.id === id))
+    .catch(
+      ({ message }) =>
+        new Error(`Não foi possível consultar o banco de dados:\n${message}`)
+    );
 
 const addTask = async (description) => {
-  if(!description) return false;
+  if (!description) return false;
 
   const newTask = {
     id: uuid(),
     description,
-    check: false
+    check: false,
   };
 
   const tasks = await getAllTasks();
@@ -25,50 +33,53 @@ const addTask = async (description) => {
 
   writeFile(`${jsonDBPath}tasks.json`, JSON.stringify(tasks, 0, 2));
 
-  return newTask
-}
+  return newTask;
+};
 
 const rmTask = async (id) => {
-  if(!id) return false;
+  if (!id) return false;
 
   const tasks = await getAllTasks();
-  const newTaskList = tasks.filter(item => item.id !== id);
+  const newTaskList = tasks.filter((item) => item.id !== id);
 
   writeFile(`${jsonDBPath}tasks.json`, JSON.stringify(newTaskList, 0, 2));
 
-  return true
-}
+  return true;
+};
 
 const putTask = async (id, description, check) => {
-  if(!id) return false;
-  
+  if (!id) return false;
+
   const tasks = await getAllTasks();
-  const newTaskList = tasks.map(item => {
+  const newTaskList = tasks.map((item) => {
     if (item.id !== id) {
       return item;
     } else {
       return {
         ...item,
         description: description || item.description,
-        check: check !== undefined ? check : item.check
-      }
+        check: check !== undefined ? check : item.check,
+      };
     }
   });
 
   writeFile(`${jsonDBPath}tasks.json`, JSON.stringify(newTaskList, 0, 2));
 
-  return true
-}
+  return true;
+};
 
 const resetTasks = async () => {
   const bkpTasks = await readFile(`${jsonDBPath}tasks.bkp.json`)
-    .then((file)=> JSON.parse(file))
-    .catch(({ message })=> new Error(`Não foi possível consultar o banco de dados:\n${message}`));
+    .then((file) => JSON.parse(file))
+    .catch(
+      ({ message }) =>
+        new Error(`Não foi possível consultar o banco de dados:\n${message}`)
+    );
 
   writeFile(`${jsonDBPath}tasks.json`, JSON.stringify(bkpTasks, 0, 2));
 
-  return true
-}
+  return true;
+};
 
 module.exports = {
   getAllTasks,
@@ -76,5 +87,5 @@ module.exports = {
   addTask,
   rmTask,
   putTask,
-  resetTasks
-}
+  resetTasks,
+};
