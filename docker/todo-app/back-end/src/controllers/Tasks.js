4112eef7bf4a99ac52c2cceb236100e1cb3b2b91
Diogo Pinto <dpintobarbosa@gmail.com>
2024-03-20 11:39:21 -0300

    Requisito 5

--- docker/todo-app/back-end/src/controllers/Tasks.js
+++ docker/todo-app/back-end/src/controllers/Tasks.js
@@ -1,51 +1,57 @@
-const taskModel = require('../models/Tasks');
+const taskModel = require("../models/Tasks");
 
-const getAllTasks = async (_req, res) => taskModel
-  .getAllTasks()
-    .then((tasks)=> res.status(200).json(tasks))
-    .catch((error)=> {
+const getAllTasks = async (_req, res) =>
+  taskModel
+    .getAllTasks()
+    .then((tasks) => res.status(200).json(tasks))
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
-const getTask = (req, res) => taskModel
-  .getTask(req.params.id)
-    .then((task)=> res.status(200).json(task))
-    .catch((error)=> {
+const getTask = (req, res) =>
+  taskModel
+    .getTask(req.params.id)
+    .then((task) => res.status(200).json(task))
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
-const addTask = (req, res) => taskModel
-  .addTask(req.body.description)
-    .then((addedTask)=> res.status(200).json(addedTask))
-    .catch((error)=> {
+const addTask = (req, res) =>
+  taskModel
+    .addTask(req.body.description)
+    .then((addedTask) => res.status(200).json(addedTask))
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
-const rmTask = (req, res) => taskModel
-  .rmTask(req.params.id)
-    .then(()=> res.status(204).end())
-    .catch((error)=> {
+const rmTask = (req, res) =>
+  taskModel
+    .rmTask(req.params.id)
+    .then(() => res.status(204).end())
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
-const putTask = ({ params, body }, res) => taskModel
-  .putTask(params.id, body.description, body.check)
-    .then(()=> res.status(204).end())
-    .catch((error)=> {
+const putTask = ({ params, body }, res) =>
+  taskModel
+    .putTask(params.id, body.description, body.check)
+    .then(() => res.status(204).end())
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
-const resetTasks = (_req, res) => taskModel
-  .resetTasks()
-    .then(()=> res.status(204).end())
-    .catch((error)=> {
+const resetTasks = (_req, res) =>
+  taskModel
+    .resetTasks()
+    .then(() => res.status(204).end())
+    .catch((error) => {
       console.error(error);
-      res.status(500).end()
+      res.status(500).end();
     });
 
 module.exports = {
@@ -54,5 +60,5 @@ module.exports = {
   addTask,
   rmTask,
   putTask,
-  resetTasks
-}
+  resetTasks,
+};
